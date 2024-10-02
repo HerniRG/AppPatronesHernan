2d36ebbb1c7c807c7e7803be65c3228e17ebf025
hernirg <hernanrg85@gmail.com>
2024-10-02 19:31:04 +0200

    tests added

--- AppPatronesHernanTests/UseCaseTests/LoginUseCaseTests.swift
+++ AppPatronesHernanTests/UseCaseTests/LoginUseCaseTests.swift
@@ -5,22 +5,69 @@
 //  Created by Hernán Rodríguez on 1/10/24.
 //
 
-import Foundation
+@testable import AppPatronesHernan
+import XCTest
 
-protocol SessionDataSourceContract {
-    func storeSession(_ session: Data)
+final class APISessionMock: APISessionContract {
+    let mockResponse: ((any APIRequest) -> Result<Data, any Error>)
     
-    func getSession() -> Data?
-}
-
-final class SessionDataSource: SessionDataSourceContract {
-    private static var token: Data?
-    
-    func storeSession(_ session: Data) {
-        SessionDataSource.token = session
+    init(mockResponse: @escaping (any APIRequest) -> Result<Data, any Error>) {
+        self.mockResponse = mockResponse
     }
     
-    func getSession() -> Data? {
-        SessionDataSource.token
+    func request<Request: APIRequest>(apiRequest: Request, completion: @escaping (Result<Data, any Error>) -> Void) {
+        completion(mockResponse(apiRequest))
+    }
+}
+
+final class DummySessionDataSource: SessionDataSourceContract {
+    private(set) var session: Data?
+        
+    func storeSession(_ session: Data) {
+        self.session = session
+    }
+    
+    func getSession() -> Data? { nil }
+
+}
+
+final class LoginUseCaseTests: XCTestCase {
+    
+    func testSuccessStoresToken() {
+        let dataSource = DummySessionDataSource()
+        let sut = LoginUseCase(dataSource: dataSource)
+        
+        let expectation = self.expectation(description: "TestSuccess")
+        let data = Data("hello-world".utf8)
+        
+        APISession.shared = APISessionMock { _ in .success(data) }
+        sut.execute(credentials: Credentials(username: "a@b.es", password: "122345")) { result in
+            guard case .success = result else {
+                return
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(dataSource.session, data)
+    }
+    
+    func testFailureDoesNotStoreToken() {
+        let dataSource = DummySessionDataSource()
+        let sut = LoginUseCase(dataSource: dataSource)
+        
+        let expectation = self.expectation(description: "TestFailure")
+        
+        APISession.shared = APISessionMock { _ in .failure(APIErrorResponse.network("login-fail")) }
+        
+        sut.execute(credentials: Credentials(username: "a@b.es", password: "123456")) { result in
+            guard case .failure = result else {
+                return
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5)
+        XCTAssertNil(dataSource.session)
     }
 }
--- AppPatronesHernanTests/ViewModelTests/HeroesListViewModelTests.swift
+++ AppPatronesHernanTests/ViewModelTests/HeroesListViewModelTests.swift
@@ -5,3 +5,63 @@
 //  Created by Hernán Rodríguez on 1/10/24.
 //
 
+@testable import AppPatronesHernan
+import XCTest
+
+private final class SuccessGetHeroesUseCaseMock: GetAllHeroesUseCaseContract {
+    func execute(completion: @escaping (Result<[AppPatronesHernan.Hero], any Error>) -> Void) {
+        completion(.success([Hero(identifier: "1234",
+                                  name: "potato",
+                                  description: "",
+                                  photo: "",
+                                  favorite: false)]))
+    }
+}
+
+private final class FailedGetHeroesUseCaseMock: GetAllHeroesUseCaseContract {
+    func execute(completion: @escaping (Result<[AppPatronesHernan.Hero], any Error>) -> Void) {
+        completion(.failure(APIErrorResponse.unknown("")))
+    }
+}
+
+
+final class HeroesListViewModelTests: XCTestCase {
+    
+    func testSuccessScenario() {
+        let successExpectation = expectation(description: "Success")
+        let loadingExpectation = expectation(description: "Loading")
+        let useCaseMock = SuccessGetHeroesUseCaseMock()
+        let sut = HeroesListViewModel(useCase: useCaseMock)
+        
+        sut.onStateChanged.bind { state in
+            if state == .loading {
+                loadingExpectation.fulfill()
+            } else if state == .success {
+                successExpectation.fulfill()
+            }
+        }
+        
+        sut.load()
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(sut.heroes.count, 1)
+    }
+    
+    func testFailScenario() {
+        let errorExpectation = expectation(description: "Error")
+        let loadingExpectation = expectation(description: "Loading")
+        let useCaseMock = FailedGetHeroesUseCaseMock()
+        let sut = HeroesListViewModel(useCase: useCaseMock)
+        
+        sut.onStateChanged.bind { state in
+            if state == .loading {
+                loadingExpectation.fulfill()
+            } else if case .error = state {
+                errorExpectation.fulfill()
+            }
+        }
+        
+        sut.load()
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(sut.heroes.count, 0)
+    }
+}
