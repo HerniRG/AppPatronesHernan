b8c87705020e4a9abfcd34509f5a130dd54f44c8
hernirg <hernanrg85@gmail.com>
2024-10-07 18:30:11 +0200

    nuevo caso de uso getSingleTranformation

---
+++ AppPatronesHernan/Domain/UseCases/GetSingleTransformationUseCase.swift
@@ -0,0 +1,40 @@
+//
+//  GetSingleTransformationUseCase.swift
+//  AppPatronesHernan
+//
+//  Created by Hernán Rodríguez on 7/10/24.
+//
+
+import Foundation
+
+// MARK: - Get Single Transformation Use Case Contract
+protocol GetSingleTransformationUseCaseContract {
+    func execute(heroId: String, transformationId: String, completion: @escaping (Result<Transformation, Error>) -> Void)
+}
+
+// MARK: - Get Single Transformation Use Case Implementation
+final class GetSingleTransformationUseCase: GetSingleTransformationUseCaseContract {
+    private let getAllTransformationsUseCase: GetTransformationUseCaseContract
+    
+    // Inicializa con el caso de uso para obtener todas las transformaciones del heroId
+    init(getAllTransformationsUseCase: GetTransformationUseCaseContract = GetTransformationUseCase()) {
+        self.getAllTransformationsUseCase = getAllTransformationsUseCase
+    }
+    
+    // Ejecuta la solicitud para obtener una transformacion con su id específico
+    func execute(heroId: String, transformationId: String, completion: @escaping (Result<Transformation, any Error>) -> Void) {
+        GetTransformationUseCase().execute(heroId: heroId) { result in
+            switch result {
+            case .success(let transformations):
+                // Filtra las transformaciones por el id que coincide
+                if let transformation = transformations.first(where: { $0.id == transformationId }) {
+                    completion(.success(transformation))}
+                else {
+                    completion(.failure(NSError(domain: "Transformation not found", code: 404, userInfo: nil)))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
